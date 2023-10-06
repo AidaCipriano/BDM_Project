9a962091934ff03b4275e6a8fb5fe87a61af17f0
AidaCipriano <aidae.ciprianoromo@gmail.com>
2023-10-06 00:03:09 -0600

    Avance2_Scripts

---
+++ Scripts/Procedures.sql
@@ -0,0 +1,99 @@
+/*Procedures*/
+ /* USE BD_BDM;*/
+
+#1. Gestion de Usuarios
+DROP procedure IF EXISTS sp_Usuarios;
+DELIMITER $$
+ CREATE PROCEDURE sp_Usuarios(
+	opc						varchar(30)		,
+    pid_usuario				int				,
+	pnombres				varchar(50)		,
+    papellidos				varchar(50)		,
+	pgenero					varchar(10)		,
+	pnacimiento				date			,
+	pemail					varchar(50)		,
+	pcontrasena				varchar(20)		,
+    prol					varchar(20)		,
+    pimagen					longblob		,
+    padministrador			bit				
+	)
+BEGIN
+DECLARE busqueda_Cuenta INT; DECLARE v_buscaremail int ;
+SET v_buscaremail = (SELECT Count(email) FROM USUARIO WHERE email = pemail);
+
+	if(opc='Registro')
+    then
+		
+         IF (v_buscaremail = 0) 
+		THEN
+			INSERT INTO USUARIO (nombres, apellidos, genero, nacimiento, email, contrasena, rol, fecharegistro, ultimamodicacion, imagen, activo, administrador)
+			VALUES 				(pnombres, papellidos, pgenero, pnacimiento, pemail, pcontrasena, prol, (SELECT NOW()), (SELECT NOW()), pimagen, 1, padministrador);
+		END IF;
+        
+        IF (v_buscaremail = 1) 
+		THEN
+				UPDATE USUARIO
+					SET
+						activo = 1
+					WHERE
+						id_usuario 	= pid_usuario ;
+				set opc='Cuenta activa';
+		END IF;
+
+	end if;
+	
+    if(opc='Actualizar')
+    then
+    		UPDATE USUARIO
+			SET
+				nombres				= 	pnombres,
+				apellidos			= 	papellidos,
+				genero				= 	pgenero,
+				nacimiento			= 	pnacimiento,
+				email				= 	pemail,
+				contrasena			= 	pcontrasena,
+				ultimamodicacion	= 	(SELECT NOW()),
+				imagen				=	pimagen	
+		WHERE
+			id_usuario 	= pid_usuario ;
+    end if;
+	
+    if(opc='Dar de baja')
+    then
+		UPDATE USUARIO
+			SET
+				activo = 0
+			WHERE
+				id_usuario 	= pid_usuario ;
+    end if;
+END$$
+
+#Iniciar sesion
+DROP procedure IF EXISTS sp_Inicio_Sesion;
+DELIMITER $$
+ CREATE PROCEDURE sp_Inicio_Sesion(
+	IN pemail					VARCHAR(50)		,
+	IN pcontrasena				VARCHAR(20)	,	
+    OUT prol VARCHAR(50)
+	) 
+BEGIN
+	DECLARE busqueda_Cuenta INT; DECLARE v_buscaremail int ; DECLARE v_buscarpasword VARCHAR(20) ; DECLARE v_id INT; DECLARE intentos tinyint;
+    declare pactivo bit;
+	SET busqueda_Cuenta =  (SELECT COUNT(id_usuario) FROM USUARIO WHERE email = pemail AND contrasena = pcontrasena);
+	SET v_buscaremail = (SELECT Count(email) FROM USUARIO WHERE email = pemail);
+    SET pactivo = (SELECT activo FROM USUARIO WHERE email = pemail);
+
+ IF (busqueda_Cuenta = 1) 
+    THEN
+		IF (pactivo = 1) 
+		THEN
+			SET prol = (SELECT rol FROM USUARIO WHERE email = pemail);
+		END IF;
+        IF (pactivo = 0) 
+		THEN
+			SET prol = "Cuenta enexistente. Favor de registrarse";
+		END IF;
+    END IF;
+    
+
+END$$
---
+++ Scripts/TABLAS.sql
@@ -0,0 +1,186 @@
+create database BD_BDM_project;
+
+USE BD_BDM_project;
+
+DROP TABLE IF EXISTS MENSAJES;
+DROP TABLE IF EXISTS CHAT;
+DROP TABLE IF EXISTS COMENTARIOS;
+DROP TABLE IF EXISTS PRODUCTO_CLIENTE;
+DROP TABLE IF EXISTS COMPRAPRODUCTO_CLIENTE;
+DROP TABLE IF EXISTS IMAGEN_PRODUCTO;
+DROP TABLE IF EXISTS VIDEO_PRODUCTO;
+DROP TABLE IF EXISTS PRODUCTO_VENDEDOR;
+DROP TABLE IF EXISTS CATEGORIA;
+DROP TABLE IF EXISTS USUARIO;
+
+
+CREATE TABLE USUARIO (
+	id_usuario				int auto_increment			not null comment "ID del Usuario",
+	nombres					varchar(50)					not null comment "Nombre o nombres del Usuario",
+    apellidos				varchar(50)					not null comment "Apellido o apellidos del Usuario",
+	genero					varchar(10)					not null comment "Genero del Usuario",
+	nacimiento				date						not null comment "Fecha de Nacimiento del Usuario",
+	email					varchar(50)					not null comment "Email del Usuario",
+	contrasena				varchar(20)					not null comment "Contrasena de la cuenta del Usuario",
+    rol						varchar(20)					not null comment "Rol del usuario: Instructor o Estudiante",
+    fecharegistro			datetime					not null  comment "Fecha en la que el Usuario se registro",
+    ultimamodicacion		datetime					not null comment "Ultima vez que el Usuario modifico su informacion",
+    imagen					longblob					null comment "Avatar/Imagen del Perfil del Usuario",
+    activo					bit							not null comment "Estado del Usuario.",
+    administrador			bool						not null comment "Si el usuario es Admin o no",
+
+	CONSTRAINT PK_USUARIO
+			PRIMARY KEY (id_usuario)
+);
+
+CREATE TABLE CATEGORIA (
+	id_categoria				int					not null comment "ID de la Categoría",
+    nombre						varchar(50)			not null comment "Nombre de la Categoria",
+	creador						int					not null comment "ID del usuario",
+    descripcion					varchar(100)		not null comment "Descripcion sobre la categoria",
+    fechahoracreacion			datetime			not null comment "Fecha de Creacion",
+
+	CONSTRAINT PK_CATEGORIA
+			PRIMARY KEY (id_categoria),
+	CONSTRAINT FK_CATEGORIA_CREADOR
+			FOREIGN KEY (creador)
+			REFERENCES USUARIO(id_usuario)
+);
+
+CREATE TABLE PRODUCTO_VENDEDOR (
+	id_producto_vendedor		int auto_increment		not null comment "ID del Producto",
+	vendedor  					int						not null comment "ID del Vendedor",
+    nombre						varchar(100)			not null comment "Nombre del Producto",
+    costo						decimal	(10,2)			not null comment "Costo del Producto",
+    descripcion					varchar(300)			not null comment "De que trata el Producto?",
+    activo						bit						not null comment "Estatus del Producto",
+    fecha_creacion				date					not null comment "Fecha de creacion del Producto",
+    cantidad_disponible			int						not null comment "Cantidad disponible del Producto",
+	cotizacion					bit						not null comment "Es para cotizar el Producto?",
+	venta						bit						not null comment "Es para venta el Producto?",
+	categoria					int						not null comment "A que categoria el Producto?",
+
+	CONSTRAINT PK_PRODUCTO_VENDEDOR
+			PRIMARY KEY (id_producto_vendedor),
+	CONSTRAINT FK_PRODUCTO_VENDEDOR_CREADOR
+			FOREIGN KEY (vendedor)
+			REFERENCES USUARIO(id_usuario),
+	CONSTRAINT FK_PRODUCTO_VENDEDOR_CATEOGORIA
+			FOREIGN KEY (categoria)
+			REFERENCES CATEGORIA(id_categoria)
+);
+
+CREATE TABLE VIDEO_PRODUCTO (
+	id_video_producto		int auto_increment		not null comment "ID del Video",
+	titulo  					int					not null comment "Titulo del video del Producto",
+    producto  					int					not null comment "ID del Producto",
+    vendedor  					int					not null comment "Vendedor del Producto",
+
+	CONSTRAINT PK_VIDEO_PRODUCTO
+			PRIMARY KEY (id_video_producto),
+	CONSTRAINT FK_VIDEO_PRODUCTO_PRODUCTO
+			FOREIGN KEY (producto)
+			REFERENCES PRODUCTO_VENDEDOR(id_producto_vendedor), 
+	CONSTRAINT FK_VIDEO_PRODUCTO_CREADOR
+			FOREIGN KEY (vendedor)
+			REFERENCES USUARIO(id_usuario)
+);
+
+CREATE TABLE IMAGEN_PRODUCTO (
+	id_imagen_producto		int auto_increment		not null comment "ID de la Imagen",
+	titulo  					int					not null comment "Titulo de la imagen del Producto",
+    producto  					int					not null comment "ID del Producto",
+    vendedor  					int					not null comment "Vendedor del Producto",
+
+	CONSTRAINT PK_IMAGEN_PRODUCTO
+			PRIMARY KEY (id_imagen_producto),
+	CONSTRAINT FK_IMAGEN_PRODUCTO_PRODUCTO
+			FOREIGN KEY (producto)
+			REFERENCES PRODUCTO_VENDEDOR(id_producto_vendedor), 
+	CONSTRAINT FK_IMAGEN_PRODUCTO_CREADOR
+			FOREIGN KEY (vendedor)
+			REFERENCES USUARIO(id_usuario)
+);
+
+CREATE TABLE COMPRAPRODUCTO_CLIENTE (
+	id_compraproducto_cliente	int auto_increment	not null comment "ID de la compra",
+	cliente  					int					not null comment "Quien es el cliente?",
+    producto  					int					not null comment "ID del Producto",
+    costo  						decimal	(10,2)		not null comment "Costo del producto",
+	fechacompra  				datetime			not null comment "Fecha de la compra",
+	formapago					varchar(50)			not null comment "Forma de pago",
+
+	CONSTRAINT PK_COMPRAPRODUCTO_CLIENTE
+			PRIMARY KEY (id_compraproducto_cliente),
+	CONSTRAINT FK_COMPRAPRODUCTO_CLIENTE_PRODUCTO
+			FOREIGN KEY (producto)
+			REFERENCES PRODUCTO_VENDEDOR(id_producto_vendedor), 
+	CONSTRAINT FK_COMPRAPRODUCTO_CLIENTE_CLIENTE
+			FOREIGN KEY (cliente)
+			REFERENCES USUARIO(id_usuario)
+);
+
+CREATE TABLE PRODUCTO_CLIENTE (
+	id_producto_cliente		int auto_increment		not null comment "ID del producto comprado por el cliente",
+	estatus  				varchar(50)				not null comment "Estatus del producto: En proceso, enviado, entregado",
+	fechaderecibido 		datetime				null comment "Fecha de la entrega del producto",
+	codigodecompra			int						not null comment "Codigo de la compra",
+
+	CONSTRAINT PK_PRODUCTO_CLIENTE
+			PRIMARY KEY (id_producto_cliente),
+	CONSTRAINT FK_PRODUCTO_CLIENTE_CODIGO
+			FOREIGN KEY (codigodecompra)
+			REFERENCES COMPRAPRODUCTO_CLIENTE(id_compraproducto_cliente)
+);
+
+CREATE TABLE COMENTARIOS (
+	id_comentarios		int auto_increment		not null comment "ID del comentario por el cliente",
+	fechapublicado		datetime				not null comment "Fecha publicado del comentario",
+    calificacion		decimal	(10,2)			not null comment "Calificacion del producto",
+    texto				varchar(300)			null comment "Texto del comentario",
+    notificacion		varchar(100)			null comment "Notificacion para comentar",
+    cliente				int						not null comment "Cliente que compro el producto y publico el comentario",
+	codigodeproducto	int						not null comment "Codigo del producto",
+
+	CONSTRAINT PK_COMENTARIOS
+			PRIMARY KEY (id_comentarios),
+	CONSTRAINT FK_COMENTARIOS_CODIGOPRODUCTO
+			FOREIGN KEY (codigodeproducto)
+			REFERENCES PRODUCTO_CLIENTE(id_producto_cliente),
+	CONSTRAINT FK_COMENTARIOS_CLIENTE
+			FOREIGN KEY (cliente)
+			REFERENCES USUARIO(id_usuario)
+);
+
+CREATE TABLE CHAT (
+	id_chat			int auto_increment		not null comment "ID del chat",
+	activo			datetime				not null comment "Chat activo o no",
+    imagen			decimal	(10,2)			not null comment "Imagen del chat",
+	codigocompra	int						not null comment "Codigo de la compra del producto",
+
+	CONSTRAINT PK_CHAT
+			PRIMARY KEY (id_chat),
+	CONSTRAINT FK_CHAT_CODIGOCOMPRAPRODUCTO
+			FOREIGN KEY (codigocompra)
+			REFERENCES COMPRAPRODUCTO_CLIENTE(id_compraproducto_cliente)
+);
+
+CREATE TABLE MENSAJES (
+	id_mensajes			int auto_increment		not null comment "ID del mensaje",
+	texto				varchar(300)			not null comment "Texto del mensaje",
+    fecha_hora			datetime				not null comment "Fecha y hora del mensaje",
+	receptor			int						not null comment "Receptor del mensaje",
+	emisor				int						not null comment "Emisor del mensaje",
+    chat				int						not null comment "Chat al que pertenece el mensaje",
+	CONSTRAINT PK_MENSAJES
+			PRIMARY KEY (id_mensajes),
+	CONSTRAINT FK_MENSAJES_CHAT
+			FOREIGN KEY (chat)
+			REFERENCES CHAT(id_chat),
+	CONSTRAINT FK_MENSAJES_RECEPTOR
+			FOREIGN KEY (receptor)
+			REFERENCES USUARIO(id_usuario),
+	CONSTRAINT FK_MENSAJES_EMISOR
+			FOREIGN KEY (emisor)
+			REFERENCES USUARIO(id_usuario)
+);
